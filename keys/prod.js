2b657f255561278ac27dc2a6f4b9d73670fc448c
kimji <kimjigabk@gmail.com>
2021-12-18 18:42:31 -0500

    fix keys

--- keys/prod.js
+++ keys/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
