4b811a316a343d5380689461a6826722d2280a46
kimji <kimjigabk@gmail.com>
2021-12-18 18:06:21 -0500

    fix keys

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSesson = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 
-const keys = require("./keys/dev");
+const keys = require('./config/keys');
 require("./models/User");
 require("./models/Character");
 require("./services/passport");
