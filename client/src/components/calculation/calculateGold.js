0f1d4a81f995cc07ef1a875ca6cd6b051bd38258
kimji <kimjigabk@gmail.com>
2021-12-22 03:38:12 -0500

    Update calcualteGold

--- client/src/components/calculation/calculateGold.js
+++ client/src/components/calculation/calculateGold.js
@@ -2,17 +2,17 @@ import { ARGOS, BALTAN, BIA, KOKOU, ABREL } from "../bossedit/bossCode";
 export const calculateGold = (pb, boss, lvl) => {
   let gold = 0;
   if (boss === ARGOS) {
-    if (pb.normal && lvl < 1475) gold += 3300;
+    if (pb.normal && lvl < 1475) gold += 1600;
     if (pb.bus) gold += parseInt(pb.busfee);
     return gold;
   }
 
   if (boss === BALTAN) {
-    if (pb.normal) gold = 3300;
+    if (pb.normal) gold = 2500;
     if (pb.hard) gold = 4500;
     if (pb.gateway) {
-      if (pb.n1) gold += 800;
-      if (pb.n2) gold += 2500;
+      if (pb.n1) gold += 500;
+      if (pb.n2) gold += 2000;
       if (pb.h1) gold += 1000;
       if (pb.h2) gold += 3500;
     }
@@ -20,15 +20,15 @@ export const calculateGold = (pb, boss, lvl) => {
     return gold;
   }
   if (boss === BIA) {
-    if (pb.normal) gold = 3300;
+    if (pb.normal) gold = 2500;
     if (pb.hard) gold = 4500;
     if (pb.gateway) {
       if (pb.n1) gold += 500;
       if (pb.n2) gold += 600;
-      if (pb.n3) gold += 2200;
-      if (pb.h1) gold += 500;
+      if (pb.n3) gold += 1400;
+      if (pb.h1) gold += 1000;
       if (pb.h2) gold += 1000;
-      if (pb.h3) gold += 3000;
+      if (pb.h3) gold += 2500;
     }
     if (pb.bus) gold += parseInt(pb.busfee);
     return gold;
@@ -36,23 +36,23 @@ export const calculateGold = (pb, boss, lvl) => {
   if (boss === KOKOU) {
     if (pb.normal) gold = 4500;
     if (pb.gateway) {
-      if (pb.n1) gold += 500;
+      if (pb.n1) gold += 1000;
       if (pb.n2) gold += 1000;
-      if (pb.n3) gold += 3000;
+      if (pb.n3) gold += 2500;
     }
     if (pb.bus) gold += parseInt(pb.busfee);
     return gold;
   }
   if (boss === ABREL) {
-    if (pb.normal) gold = 7500;
-    if (pb.hard) gold = 9500;
+    if (pb.normal) gold = 8500;
+    if (pb.hard) gold = 10500;
     if (pb.gateway) {
       if (pb.n1) gold += 4500;
       if (pb.n2) gold += 1500;
-      if (pb.n3) gold += 1500;
+      if (pb.n3) gold += 2500;
       if (pb.h1) gold += 5500;
       if (pb.h2) gold += 2000;
-      if (pb.h3) gold += 2000;
+      if (pb.h3) gold += 3000;
     }
     if (pb.bus) gold += parseInt(pb.busfee);
     return gold;
